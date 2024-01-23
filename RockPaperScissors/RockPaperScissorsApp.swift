9afbe6dc4756e9b28d7e64fd0243cfaa441f5524
Nick Krasnitskiy <nick.krasnitskiy@gmail.com>
2024-01-23 10:41:03 +0300

    Initial Commit

---
+++ RockPaperScissors/RockPaperScissorsApp.swift
@@ -0,0 +1,17 @@
+//
+//  RockPaperScissorsApp.swift
+//  RockPaperScissors
+//
+//  Created by Nick Krasnitskiy on 23.01.2024.
+//
+
+import SwiftUI
+
+@main
+struct RockPaperScissorsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

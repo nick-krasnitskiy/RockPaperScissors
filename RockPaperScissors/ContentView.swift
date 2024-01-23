ac2ea028dc790518df6076e42d21385c80f067b0
Nick Krasnitskiy <nick.krasnitskiy@gmail.com>
2024-01-23 11:19:18 +0300

    Create base UI

--- RockPaperScissors/ContentView.swift
+++ RockPaperScissors/ContentView.swift
@@ -8,14 +8,44 @@
 import SwiftUI
 
 struct ContentView: View {
+    let moves = ["diamond.fill", "newspaper", "scissors"]
+    let alternatives = ["Win", "Lose"]
+    
+    var randomElement: Int {
+        Int.random(in: 0...2)
+    }
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        VStack(spacing: 30) {
+            Spacer()
+            
+            Text(alternatives.randomElement() ?? "")
+                .font(.largeTitle).bold()
+                .foregroundStyle(.indigo)
+            Image(systemName: moves[randomElement])
+                .resizable()
+                .frame(width: 110, height: 100)
+                .foregroundStyle(.indigo)
+            
+            Spacer()
+            
+            HStack(spacing: 50) {
+                ForEach(moves, id: \.self) { number in
+                    Button {} label: {
+                        Image(systemName: number)
+                            .resizable()
+                            .frame(width: 50, height: 50)
+                            .foregroundStyle(.white)
+                    }
+                    .padding()
+                    .background(.indigo)
+                    .clipShape(.circle)
+                  
+                }
+            }
+            
+            Spacer()
         }
-        .padding()
     }
 }
 
